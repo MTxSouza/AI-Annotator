226cb4aa9b8621d0b7797be7862e7d8b5c3393f9
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2026-01-25 20:51:39 -0300

    fix: Overwrite the prod lifespan to test lifespan.

--- tests/backend/conftest.py
+++ tests/backend/conftest.py
@@ -1,10 +1,15 @@
 """
 Main module that setup main configurations for backend tests.
 """
+from contextlib import asynccontextmanager
+
 import pytest
+from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
 from backend.app import app
+from backend.configs import BackendSettings
+from backend.database.configs import DatabaseConfig
 
 
 # Session-wide fixtures.
@@ -13,6 +18,24 @@ def client():
     """
     Fixture to provide the API client for tests.
     """
+    # Test lifespan context.
+    @asynccontextmanager
+    async def test_lifespan(app: FastAPI):
+
+        # Initialize the database client.
+        await DatabaseConfig.initialize_client(
+            uri=BackendSettings.database_uri,
+            database_name=BackendSettings.database_name,
+            port=BackendSettings.database_port
+        )
+        await DatabaseConfig._drop_database()  # Clean database before tests.
+        yield
+
+        # Close the database client.
+        await DatabaseConfig.close_client()
+
+    app.router.lifespan_context = test_lifespan
+
     # Provide the API client for tests.
     with TestClient(app=app) as client:
 
