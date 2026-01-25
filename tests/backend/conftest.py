856e0257ed5de2aef8ba4d28f08e9641fea42aea
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2026-01-25 14:24:15 -0300

    update: Run health check before allow to use the test client of API.

--- tests/backend/conftest.py
+++ tests/backend/conftest.py
@@ -1,3 +1,25 @@
 """
 Main module that setup main configurations for backend tests.
 """
+import pytest
+from fastapi.testclient import TestClient
+
+from backend.app import app
+
+
+# Session-wide fixtures.
+@pytest.fixture(scope="session", autouse=True)
+def client():
+    """
+    Fixture to provide the API client for tests.
+    """
+    # Provide the API client for tests.
+    with TestClient(app=app) as client:
+
+        # Health check before running tests.
+        response = client.get(url="/health")
+        assert response.status_code == 200
+        health_check_response = response.json()
+        assert health_check_response.get("ok") is True
+
+        yield client
