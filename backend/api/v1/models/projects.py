d1d1a2da9bc748e694fc05ae8f89fba3724080e8
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2026-01-25 11:58:25 -0300

    update: Add new project model with more detailed informations.

--- backend/api/v1/models/projects.py
+++ backend/api/v1/models/projects.py
@@ -1,6 +1,8 @@
 """
 Main module with all schemas used in Projects collection.
 """
+from typing import Optional
+
 from pydantic import Field
 
 from backend.database.models import (CommonRequestModel, CommonResponseModel,
@@ -14,8 +16,15 @@ class Project(CommonResponseModel):
     Project model.
     """
     name: str = Field(..., description="The name of the project.")
-    description: str | None = Field(default=None, description="The description of the project.")
+    description: Optional[str] = Field(default=None, description="The description of the project.")
     task_type: TaskType = Field(..., description="The type of task for the project.")
+
+class ProjectDetail(Project):
+    """
+    Project model with detailed information.
+    """
+    num_samples: Optional[int] = Field(default=0, description="The number of samples in the project.")
+    num_annotations: Optional[int] = Field(default=0, description="The number of annotations in the project.")
 
 class ProjectCreate(CommonRequestModel):
     """
@@ -28,5 +37,5 @@ class ProjectUpdate(CommonUpdateModel):
     """
     Project update model.
     """
-    name: str | None = Field(default=None, description="The name of the project.")
-    description: str | None = Field(default=None, description="The description of the project.")
+    name: Optional[str] = Field(default=None, description="The name of the project.")
+    description: Optional[str] = Field(default=None, description="The description of the project.")
