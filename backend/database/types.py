ba3900ba3070498c2be22990c3caacd1faa85b3f
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2026-01-25 11:45:05 -0300

    fix: Fix way to convert the ObjectID to string and vise and versa.

--- backend/database/types.py
+++ backend/database/types.py
@@ -2,9 +2,11 @@
 Module with custom types for database operations.
 """
 from enum import Enum
+from typing import Any
 
 from bson import ObjectId
 from pydantic.json_schema import GetJsonSchemaHandler, JsonSchemaValue
+from pydantic_core import core_schema
 
 
 # Types.
@@ -24,10 +26,24 @@ class PyObjectId(ObjectId):
         return ObjectId(v)
 
     @classmethod
+    def __get_pydantic_core_schema__(cls, source_type: Any, handler: Any) -> core_schema.CoreSchema:
+        return core_schema.json_or_python_schema(
+            json_schema=core_schema.str_schema(),
+            python_schema=core_schema.union_schema([
+                core_schema.is_instance_schema(ObjectId),
+                core_schema.chain_schema([
+                    core_schema.str_schema(),
+                    core_schema.no_info_plain_validator_function(cls.validate),
+                ])
+            ]),
+            serialization=core_schema.plain_serializer_function_ser_schema(
+                lambda x: str(x)
+            )
+        )
+
+    @classmethod
     def __get_pydantic_json_schema__(cls, core_schema: JsonSchemaValue, handler: GetJsonSchemaHandler) -> JsonSchemaValue:
-        json_schema = handler(core_schema)
-        json_schema.update(type="string")
-        return json_schema
+        return {"type": "string"}
 
 class TaskType(str, Enum):
     """
