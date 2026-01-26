7ab6f095db3743164a0523bec0f4c8db0e5a3410
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2026-01-25 21:34:33 -0300

    create: Add simple tests to test the project creation.

---
+++ tests/backend/00_projects_test.py
@@ -0,0 +1,63 @@
+"""
+Module used to test project-related endpoints.
+"""
+import pytest
+from fastapi.testclient import TestClient
+
+
+# Mocks.
+@pytest.fixture
+def project_payload() -> dict:
+    """
+    Fixture to provide a sample project payload.
+    """
+    return {
+        "name": "Test Project",
+        "task_type": "Object Detection"
+    }
+
+@pytest.fixture
+def private_project_payload() -> dict:
+    """
+    Fixture to provide a sample private project payload.
+    """
+    return {
+        "name": "Private Test Project",
+        "task_type": "Object Detection",
+        "password": "securepassword"
+    }
+
+# Tests.
+def test_create_project(client: TestClient, project_payload: dict):
+    """
+    Test the create project endpoint.
+    """
+    # Send a POST request to create the project.
+    response = client.post(url="/projects/", json=project_payload)
+
+    # Assert the response status code.
+    assert response.status_code == 201
+
+    # Assert the response data.
+    response_data = response.json()
+    assert response_data["name"] == project_payload["name"]
+    assert response_data["task_type"] == project_payload["task_type"]
+    assert response_data["description"] is None
+    assert response_data["is_private"] is False
+
+def test_create_project_with_password(client: TestClient, private_project_payload: dict):
+    """
+    Test the create project endpoint with a password.
+    """
+    # Send a POST request to create the project.
+    response = client.post(url="/projects/", json=private_project_payload)
+
+    # Assert the response status code.
+    assert response.status_code == 201
+
+    # Assert the response data.
+    response_data = response.json()
+    assert response_data["name"] == private_project_payload["name"]
+    assert response_data["task_type"] == private_project_payload["task_type"]
+    assert response_data["description"] is None
+    assert response_data["is_private"] is True
