bcd0f7038a2c2a28f8a9c0e1d021fc04df8eaa96
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2026-01-31 20:39:33 -0300

    create: Add utils module for task configs.

---
+++ backend/api/v1/utils/task_configs.py
@@ -0,0 +1,42 @@
+"""
+Module with all utilities related to task configuration operations.
+"""
+from backend.api.v1.models.task_configs import (ObjectDetectionTaskConfig,
+                                                SemanticSegmentationTaskConfig)
+from backend.database.types import PyObjectId, Task
+
+
+# Functions.
+def setup_task_config(project_id: str, task: Task) -> dict:
+    """
+    Utility function to setup the task configuration based on the task type.
+
+    Args:
+        project_id (str): The project ID.
+        task (Task): The task type.
+
+    Returns:
+        dict: The task configuration template.
+    """
+    # Task configuration mapping.
+    task_config_mapping = {
+        Task.OBJECT_DETECTION.value: ObjectDetectionTaskConfig,
+        Task.SEMANTIC_SEGMENTATION.value: SemanticSegmentationTaskConfig
+    }
+
+    # Fix task enum if needed.
+    if isinstance(task, Task):
+        task = task.value
+
+    # Get the task configuration class based on the task type.
+    task_config_class = task_config_mapping.get(task)
+
+    if not task_config_class:
+        raise ValueError("Unsupported task type: %s" % task)
+
+    # Create task configuration instance.
+    task_config_instance = task_config_class(project_id=project_id)
+    task_config_instance = task_config_instance.model_dump()
+    task_config_instance["project_id"] = PyObjectId(oid=project_id)  # Ensure project_id is PyObjectId.
+
+    return task_config_instance
