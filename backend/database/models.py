a105a65ec3086a16a142c4ce1f49353cadfcf0d6
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2026-01-31 21:45:32 -0300

    fix: Inheriting the timestemp model in create and update schema.

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,6 +1,8 @@
 """
 Module with common database models inherited by all API models.
 """
+from typing import Optional
+
 from pydantic import BaseModel, ConfigDict, Field
 
 from backend.database.types import PyDateTime, PyObjectId
@@ -12,6 +14,7 @@ class _TimestampModel(BaseModel):
     """
     Base timestamp model inherited by all API models.
     """
+    # Fields.
     created_at: PyDateTime = Field(default_factory=get_current_datetime, description="The creation timestamp.")
     updated_at: PyDateTime = Field(default_factory=get_current_datetime, description="The last update timestamp.")
 
@@ -19,22 +22,29 @@ class CommonResponseModel(_TimestampModel):
     """
     Common response model inherited by all API response models.
     """
+    # Fields.
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
 
+    # Config.
     model_config = ConfigDict(
         populate_by_name=True,
         from_attributes=True,
         arbitrary_types_allowed=True
     )
 
-class CommonRequestModel(BaseModel):
+class CommonRequestModel(_TimestampModel):
     """
     Common request model inherited by all API request models.
     """
+    # Config.
     model_config = ConfigDict(extra="forbid")
 
-class CommonUpdateModel(BaseModel):
+class CommonUpdateModel(_TimestampModel):
     """
     Common update model inherited by all API update models.
     """
+    # To be excluded.
+    created_at: Optional[PyDateTime] = Field(default=None, exclude=True)
+
+    # Config.
     model_config = ConfigDict(extra="forbid")
