6d642a2edffefab2f5796004c18e89ab7302a60e
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2026-02-02 22:02:31 -0300

    update: Fix way to corrupt the image and add new test to upload an invalid file format.

--- tests/backend/01_image_files_test.py
+++ tests/backend/01_image_files_test.py
@@ -70,9 +70,27 @@ def corrupt_image_file_payload() -> list[tuple[str, tuple[str, io.BytesIO, str]]
     Fixture to provide a corrupt image file payload.
     """
     # Create corrupt image bytes.
-    buffer = io.BytesIO(b"This is not a valid image file.")
+    corrupt_image = Image.new("RGB", (100, 100), color=(0, 255, 0))
+    buffer = io.BytesIO()
+    corrupt_image.save(buffer, format="PNG")
 
-    return [("file_list", ("corrupt_image.png", buffer, "image/png"))]
+    # Corrupt the image bytes.
+    valid_bytes = buffer.getvalue()
+    corrupt_bytes = valid_bytes[:10] + b"corrupted_data"
+
+    corrupt_buffer = io.BytesIO(corrupt_bytes)
+
+    return [("file_list", ("corrupt_image.png", corrupt_buffer, "image/png"))]
+
+@pytest.fixture
+def invalid_file_format_payload() -> list[tuple[str, tuple[str, io.BytesIO, str]]]:
+    """
+    Fixture to provide an invalid file format payload.
+    """
+    # Create text file bytes.
+    buffer = io.BytesIO(b"This is a text file, not an image.")
+
+    return [("file_list", ("invalid_file.txt", buffer, "text/plain"))]
 
 # Tests.
 def test_create_image_file_record(
@@ -157,3 +175,29 @@ def test_create_corrupt_image_file_record(
     for i, file_data in enumerate(iterable=file_response_json["data"]):
         assert file_data["status"] == "Failed"
         assert file_data["message"] == "Corrupted file: %s." % corrupt_image_file_payload[i][1][0]
+
+def test_create_invalid_file_format_record(
+    client: TestClient,
+    project_payload: dict,
+    invalid_file_format_payload: list[tuple[str, tuple[str, io.BytesIO, str]]],
+    reset_file_directory: None  # Used to reset file directory
+    ):
+    """
+    Test to create an invalid file format record.
+    """
+    # Create project first.
+    project_response = client.post(url="/projects/", json=project_payload)
+    assert project_response.status_code == 201
+    project_id = project_response.json()["_id"]
+
+    # Create file record.
+    file_response = client.post(url="/files/%s/images/" % project_id, files=invalid_file_format_payload)
+    assert file_response.status_code == 201
+
+    # Check response.
+    file_response_json = file_response.json()
+    assert "data" in file_response_json
+    assert len(file_response_json["data"]) == len(invalid_file_format_payload)
+    for i, file_data in enumerate(iterable=file_response_json["data"]):
+        assert file_data["status"] == "Failed"
+        assert file_data["message"] == "Invalid file format for this type of file: txt."
