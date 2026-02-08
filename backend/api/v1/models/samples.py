9a2ac0103ef24a5cf6fdcec17e879b9953f62d48
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2026-02-08 09:47:29 -0300

    ♻️ refactor: Reorganize inheritance for object detection samples.

--- backend/api/v1/models/samples.py
+++ backend/api/v1/models/samples.py
@@ -2,26 +2,34 @@
 Main module with all schemas used in Samples collection.
 """
 
-from pydantic import Field
+from pydantic import BaseModel, Field
 
 from backend.database.enums import PyObjectId
-from backend.database.models import CommonResponseModel
+from backend.database.models import CommonRequestModel, CommonResponseModel
 
 
 # Schemas.
-class __Sample(CommonResponseModel):
+class Sample_DB(CommonResponseModel):
     """
     Sample model in the database.
     """
 
     # Fields.
     project_id: PyObjectId | str = Field(..., description="The ID of the project the sample belongs to.")
+
+
+class Sample(BaseModel):
+    """
+    Sample model.
+    """
+
+    # Fields.
     file_id: PyObjectId | str = Field(..., description="The ID of the file associated with the sample.")
 
 
-class ObjectSample_DB(__Sample):
+class ObjectSample(Sample):
     """
-    Object sample model in the database.
+    Object sample model.
     """
 
     # Fields.
@@ -29,9 +37,9 @@ class ObjectSample_DB(__Sample):
 
 
 # - Object Detection.
-class ObjectDetectionSample_DB(ObjectSample_DB):
+class ObjectDetectionSample(ObjectSample):
     """
-    Object detection sample model in the database.
+    Object detection sample model for API responses.
     """
 
     # Fields.
@@ -45,18 +53,17 @@ class ObjectDetectionSample_DB(ObjectSample_DB):
     height: float = Field(..., ge=0.0, le=1.0, description="The height of the bounding box (normalized).")
 
 
-class ObjectDetectionSample_Create(ObjectDetectionSample_DB):
+class ObjectDetectionSample_Create(CommonRequestModel, ObjectDetectionSample):
     """
     Object detection sample model for creating new samples.
     """
 
-    # Fields.
-    file_id: PyObjectId | str = Field(..., description="The ID of the file associated with the sample.")
+    pass
 
 
-class ObjectDetectionSample(ObjectDetectionSample_DB):
+class ObjectDetectionSample_DB(Sample_DB, ObjectDetectionSample):
     """
-    Object detection sample model for API responses.
+    Object detection sample model in the database.
     """
 
     pass
