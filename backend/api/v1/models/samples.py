fad5ccec7ebf56f8b9b8a6cf8a1d08058db354b5
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2026-02-07 21:48:35 -0300

    ✨ feat: Create initial model schame to store annotations for object detection task.

---
+++ backend/api/v1/models/samples.py
@@ -0,0 +1,44 @@
+"""
+Main module with all schemas used in Samples collection.
+"""
+
+from pydantic import BaseModel, Field
+
+from backend.database.types import PyObjectId
+
+
+# Schemas.
+class __Sample(BaseModel):
+    """
+    Sample model in the database.
+    """
+
+    # Fields.
+    project_id: PyObjectId = Field(..., description="The ID of the project the sample belongs to.")
+    file_id: PyObjectId = Field(..., description="The ID of the file associated with the sample.")
+
+
+class ObjectSample_DB(__Sample):
+    """
+    Object sample model in the database.
+    """
+
+    # Fields.
+    class_name: str = Field(..., description="The name of the class for the object sample.")
+
+
+# - Object Detection.
+class ObjectDetectionSample_DB(ObjectSample_DB):
+    """
+    Object detection sample model in the database.
+    """
+
+    # Fields.
+    cx: float = Field(
+        ..., ge=0.0, le=1.0, description="The x-coordinate of the center of the bounding box (normalized)."
+    )
+    cy: float = Field(
+        ..., ge=0.0, le=1.0, description="The y-coordinate of the center of the bounding box (normalized)."
+    )
+    width: float = Field(..., ge=0.0, le=1.0, description="The width of the bounding box (normalized).")
+    height: float = Field(..., ge=0.0, le=1.0, description="The height of the bounding box (normalized).")
