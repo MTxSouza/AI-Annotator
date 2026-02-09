37a8ae983e001c2cdf97b3c38e18efe8969afdd0
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2026-02-09 18:37:43 -0300

    ✅ test: Add test to validate error during corrupted text file input.

--- tests/backend/02_text_files_test.py
+++ tests/backend/02_text_files_test.py
@@ -30,6 +30,23 @@ def list_text_file_payload() -> list[tuple[str, tuple[str, io.BytesIO, str]]]:
     return text_file_list
 
 
+@pytest.fixture
+def corrupt_text_file_payload() -> list[tuple[str, tuple[str, io.BytesIO, str]]]:
+    """
+    Fixture to provide a corrupt text file payload.
+    """
+    # Create corrupt text content.
+    valid_uft8_bom = b"\xef\xbb\xbf"
+    invalid_content = b"Corrupted file -> \xff\xfe\x00\x00"
+    corrupt_content_bytes = valid_uft8_bom + invalid_content
+
+    # Store corrupt text in bytes buffer.
+    buffer = io.BytesIO(initial_bytes=corrupt_content_bytes)
+    buffer.seek(0)
+
+    return [("file_list", ("corrupt_file.txt", buffer, "text/plain"))]
+
+
 # Tests.
 def test_create_text_file_record(
     client: TestClient,
@@ -105,3 +122,31 @@ def test_create_duplicate_text_file_record(
     for i, file_data in enumerate(iterable=duplicate_file_response_json["data"]):
         assert file_data["status"] == "Skipped"
         assert file_data["message"] == f"File '{list_text_file_payload[i][1][0]}' already exists."
+
+
+def test_create_corrupt_text_file_record(
+    client: TestClient,
+    text_project_payload: dict,
+    corrupt_text_file_payload: list[tuple[str, tuple[str, io.BytesIO, str]]],
+    reset_file_directory: None,  # Used to reset file directory
+):
+    """
+    Test to create a corrupt text file record.
+    """
+    # Create project first.
+    project_response = client.post(url="/projects/", json=text_project_payload)
+    assert project_response.status_code == 201
+    project = project_response.json()
+    project_id = project["_id"]
+
+    # Create file record.
+    file_response = client.post(url=f"/files/{project_id}/", files=corrupt_text_file_payload)
+    assert file_response.status_code == 201
+
+    # Check response.
+    file_response_json = file_response.json()
+    assert "data" in file_response_json
+    assert len(file_response_json["data"]) == len(corrupt_text_file_payload)
+    for i, file_data in enumerate(iterable=file_response_json["data"]):
+        assert file_data["status"] == "Failed"
+        assert file_data["message"] == f"Corrupted file: {corrupt_text_file_payload[i][1][0]}."
