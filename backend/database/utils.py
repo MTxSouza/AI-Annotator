4eb7edc888f0f52b293b2b38165f5517c46e12d8
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2026-02-13 22:39:14 -0300

    ♻️ refactor: Move method to get the file expected for each task to a isolated function.

--- backend/database/utils.py
+++ backend/database/utils.py
@@ -4,6 +4,8 @@ Module with basic functions to be used in database operations.
 
 from datetime import UTC, datetime
 
+from backend.database.enums import Task
+
 
 # Functions.
 def get_current_datetime() -> datetime:
@@ -14,3 +16,26 @@ def get_current_datetime() -> datetime:
             datetime: The current date and time.
     """
     return datetime.now(tz=UTC)
+
+
+def get_task_file(task: str) -> str | None:
+    """
+    Get the associated file type for a given task.
+
+    Args:
+            task (str): The task to get the file type for.
+
+    Returns:
+            str | None: The associated file type for the task, or None if not found.
+    """
+    task_file_map = {
+        Task.OBJECT_DETECTION.value: "image",
+        Task.IMAGE_CLASSIFICATION.value: "image",
+        Task.IMAGE_CAPTION.value: "image",
+        Task.OBJECT_CAPTION.value: "image",
+        Task.TEXT_CLASSIFICATION.value: "text",
+        Task.TEXT_TAGGING.value: "text",
+        Task.AUDIO_CLASSIFICATION.value: "audio",
+        Task.AUDIO_CAPTION.value: "audio",
+    }
+    return task_file_map.get(task)
