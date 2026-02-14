2dfee3ec4b126520b98d92003e3c553d5746405b
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2026-02-13 22:47:41 -0300

    ♻️ refactor: Move task functions to task config utils module.

--- backend/api/v1/utils/task_configs.py
+++ backend/api/v1/utils/task_configs.py
@@ -2,8 +2,8 @@
 Module with all utilities related to task configuration operations.
 """
 
+from backend.api.v1.models.task_configs import ObjectDetectionTaskConfig, TextClassificationTaskConfig
 from backend.database.enums import PyObjectId, Task
-from backend.database.utils import get_task_config_model_schema
 
 
 # Functions.
@@ -36,3 +36,43 @@ def setup_task_config(project_id: str, task: Task) -> dict:
     task_config_instance_dict["project_id"] = PyObjectId(oid=project_id)  # Ensure project_id is PyObjectId.
 
     return task_config_instance_dict
+
+
+def get_task_file(task: str) -> str | None:
+    """
+    Get the associated file type for a given task.
+
+    Args:
+            task (str): The task to get the file type for.
+
+    Returns:
+            str | None: The associated file type for the task, or None if not found.
+    """
+    task_file_map = {
+        Task.OBJECT_DETECTION.value: "image",
+        Task.IMAGE_CLASSIFICATION.value: "image",
+        Task.IMAGE_CAPTION.value: "image",
+        Task.OBJECT_CAPTION.value: "image",
+        Task.TEXT_CLASSIFICATION.value: "text",
+        Task.TEXT_TAGGING.value: "text",
+        Task.AUDIO_CLASSIFICATION.value: "audio",
+        Task.AUDIO_CAPTION.value: "audio",
+    }
+    return task_file_map.get(task)
+
+
+def get_task_config_model_schema(task: str) -> type | None:
+    """
+    Get the associated task configuration model schema for a given task.
+
+    Args:
+            task (str): The task to get the configuration model schema for.
+
+    Returns:
+            type | None: The associated task configuration model schema for the task, or None if not found.
+    """
+    task_config_model_map = {
+        Task.OBJECT_DETECTION.value: ObjectDetectionTaskConfig,
+        Task.TEXT_CLASSIFICATION.value: TextClassificationTaskConfig,
+    }
+    return task_config_model_map.get(task)
