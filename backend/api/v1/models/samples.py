a19e08c75bc5d18e04080da83bdc285039d54f7a
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2026-02-13 22:51:17 -0300

    ✨ feat: Reorganize the inheritance of classes.

--- backend/api/v1/models/samples.py
+++ backend/api/v1/models/samples.py
@@ -5,99 +5,226 @@ Main module with all schemas used in Samples collection.
 from pydantic import Field
 
 from backend.database.enums import PyObjectId
-from backend.database.models import CommonModel, CommonRequestModel, CommonResponseModel
+from backend.database.models import CommonModel, CommonRequestModel, CommonResponseModel, CommonUpdateModel
 
 
 # Schemas.
-class Sample_DB(CommonResponseModel):
+class _Sample(CommonModel):
     """
-    Sample model in the database.
-    """
-
-    # Fields.
-    project_id: PyObjectId | str = Field(..., description="The ID of the project the sample belongs to.")
-
-
-class Sample(CommonModel):
-    """
-    Sample model.
+    Schema for a sample.
     """
 
-    # Fields.
-    file_id: PyObjectId | str = Field(..., description="The ID of the file associated with the sample.")
+    project_id: str | PyObjectId = Field(..., description="ID of the project associated with the sample.")
+    file_id: str | PyObjectId = Field(..., description="ID of the file associated with the sample.")
 
 
-class TextSample(Sample):
+class _SampleDB(CommonResponseModel, _Sample):
     """
-    Text sample model.
-    """
-
-    # Fields.
-    text: str | None = Field(default=None, description="The text content of the sample.")
-
-
-class ClassificationSample(Sample):
-    """
-    Classification sample model.
-    """
-
-    # Fields.
-    class_name: str = Field(..., description="The name of the class for the classification sample.")
-
-
-# - Object Detection.
-class ObjectDetectionSample(ClassificationSample):
-    """
-    Object detection sample model for API responses.
-    """
-
-    # Fields.
-    cx: float = Field(
-        ..., ge=0.0, le=1.0, description="The x-coordinate of the center of the bounding box (normalized)."
-    )
-    cy: float = Field(
-        ..., ge=0.0, le=1.0, description="The y-coordinate of the center of the bounding box (normalized)."
-    )
-    width: float = Field(..., ge=0.0, le=1.0, description="The width of the bounding box (normalized).")
-    height: float = Field(..., ge=0.0, le=1.0, description="The height of the bounding box (normalized).")
-
-
-class ObjectDetectionSample_Create(CommonRequestModel, ObjectDetectionSample):
-    """
-    Object detection sample model for creating new samples.
+    Schema for a sample in the database.
     """
 
     pass
 
 
-class ObjectDetectionSample_DB(Sample_DB, ObjectDetectionSample):
+class _SampleCreate(_Sample, CommonRequestModel):
     """
-    Object detection sample model in the database.
-    """
-
-    pass
-
-
-# - Text Classification.
-class TextClassificationSample(ClassificationSample, TextSample):
-    """
-    Text classification sample model for API responses.
+    Schema for creating a sample.
     """
 
     pass
 
 
-class TextClassificationSample_Create(CommonRequestModel, TextClassificationSample):
+class _SampleUpdate(CommonUpdateModel):
     """
-    Text classification sample model for creating new samples.
+    Schema for updating a sample.
     """
 
     pass
 
 
-class TextClassificationSample_DB(Sample_DB, TextClassificationSample):
+# - Schemas for different sample types.
+class _VisualLocationSample(_Sample):
     """
-    Text classification sample model in the database.
+    Schema for a visual location sample.
+    """
+
+    cx: float = Field(..., description="X-coordinate of the center of the bounding box.")
+    cy: float = Field(..., description="Y-coordinate of the center of the bounding box.")
+    width: float = Field(..., description="Width of the bounding box.")
+    height: float = Field(..., description="Height of the bounding box.")
+
+
+class _VisualLocationSampleUpdate(_SampleUpdate):
+    """
+    Schema for updating a visual location sample.
+    """
+
+    cx: float | None = Field(default=None)
+    cy: float | None = Field(default=None)
+    width: float | None = Field(default=None)
+    height: float | None = Field(default=None)
+
+
+class _ClassSample(_Sample):
+    """
+    Schema for a class sample.
+    """
+
+    class_name: str = Field(..., description="Name of the class associated with the sample.")
+
+
+class _ClassSampleUpdate(_SampleUpdate):
+    """
+    Schema for updating a class sample.
+    """
+
+    class_name: str | None = Field(default=None)
+
+
+class _TextSample(_Sample):
+    """
+    Schema for a text sample.
+    """
+
+    text: str = Field(..., description="Text associated with the sample.")
+
+
+class _TextSampleUpdate(_SampleUpdate):
+    """
+    Schema for updating a text sample.
+    """
+
+    text: str | None = Field(default=None)
+
+
+# - Image Task Samples.
+# * Image Classification Sample.
+class _ImageClassificationSample(_ClassSample):
+    """
+    Schema for an image classification sample.
+    """
+
+    pass
+
+
+class ImageClassificationSample(_SampleDB, _ImageClassificationSample):
+    """
+    Schema for an image classification sample.
+    """
+
+    pass
+
+
+class ImageClassificationSampleCreate(_SampleCreate, _ImageClassificationSample):
+    """
+    Schema for creating an image classification sample.
+    """
+
+    pass
+
+
+class ImageClassificationSampleUpdate(_ClassSampleUpdate, _ImageClassificationSample):
+    """
+    Schema for updating an image classification sample.
+    """
+
+    pass
+
+
+# * Object Detection Sample.
+class _ObjectDetectionSample(_ClassSample, _VisualLocationSample):
+    """
+    Schema for an object detection sample.
+    """
+
+    pass
+
+
+class ObjectDetectionSample(_SampleDB, _ObjectDetectionSample):
+    """
+    Schema for an object detection sample.
+    """
+
+    pass
+
+
+class ObjectDetectionSampleCreate(_SampleCreate, _ObjectDetectionSample):
+    """
+    Schema for creating an object detection sample.
+    """
+
+    pass
+
+
+class ObjectDetectionSampleUpdate(_ObjectDetectionSample, _VisualLocationSampleUpdate):
+    """
+    Schema for updating an object detection sample.
+    """
+
+    pass
+
+
+# * Image Caption Sample.
+class _ImageCaptionSample(_TextSample):
+    """
+    Schema for an image caption sample.
+    """
+
+    pass
+
+
+class ImageCaptionSample(_SampleDB, _ImageCaptionSample):
+    """
+    Schema for an image caption sample.
+    """
+
+    pass
+
+
+class ImageCaptionSampleCreate(_SampleCreate, _ImageCaptionSample):
+    """
+    Schema for creating an image caption sample.
+    """
+
+    pass
+
+
+class ImageCaptionSampleUpdate(_ImageCaptionSample, _TextSampleUpdate):
+    """
+    Schema for updating an image caption sample.
+    """
+
+    pass
+
+
+# * Object Caption Sample.
+class _ObjectCaptionSample(_TextSample, _VisualLocationSample):
+    """
+    Schema for an object caption sample.
+    """
+
+    pass
+
+
+class ObjectCaptionSample(_SampleDB, _ObjectCaptionSample):
+    """
+    Schema for an object caption sample.
+    """
+
+    pass
+
+
+class ObjectCaptionSampleCreate(_SampleCreate, _ObjectCaptionSample):
+    """
+    Schema for creating an object caption sample.
+    """
+
+    pass
+
+
+class ObjectCaptionSampleUpdate(_ObjectCaptionSample, _VisualLocationSampleUpdate, _TextSampleUpdate):
+    """
+    Schema for updating an object caption sample.
     """
 
     pass
