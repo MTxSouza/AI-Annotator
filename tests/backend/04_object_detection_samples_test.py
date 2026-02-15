6d5cc11bbbffbc9fa5767bf091527f91dc29c113
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2026-02-15 11:26:09 -0300

    ✅ test: Test other functionalities to interact to object detection samples.

--- tests/backend/04_object_detection_samples_test.py
+++ tests/backend/04_object_detection_samples_test.py
@@ -197,3 +197,219 @@ def test_update_object_detection_sample(
         assert updated_sample_data_returned["cy"] == updated_sample_data["cy"]
         assert updated_sample_data_returned["width"] == created_sample_data["width"]
         assert updated_sample_data_returned["height"] == created_sample_data["height"]
+
+
+def test_update_object_detection_sample_with_wrong_project_id(
+    client: TestClient,
+    image_project_payload: dict,
+    image_file_payload: list[tuple[str, tuple[str, bytes, str]]],
+    list_object_detection_sample_payload: tuple[list[dict], list[str]],
+    reset_file_directory: None,  # Used to reset file directory
+) -> None:
+    """
+    Test to update an object detection sample with wrong project ID.
+    """
+    # Create project first.
+    project_response = client.post(url="/projects/", json=image_project_payload)
+    assert project_response.status_code == 201, f"Failed to create project: {project_response.text}"
+    project = project_response.json()
+    project_id = project["_id"]
+
+    # Create second project to get its ID.
+    second_image_project_payload = image_project_payload.copy()
+    second_image_project_payload["name"] = "Second Project"
+
+    second_project_response = client.post(url="/projects/", json=second_image_project_payload)
+    assert second_project_response.status_code == 201, (
+        f"Failed to create second project: {second_project_response.text}"
+    )
+    second_project = second_project_response.json()
+    second_project_id = second_project["_id"]
+
+    # Create file record.
+    file_response = client.post(url=f"/projects/{project_id}/files/", files=image_file_payload)
+    assert file_response.status_code == 201, f"Failed to create file: {file_response.text}"
+    file_list = file_response.json()["data"]
+    assert len(file_list) == 1
+    file_id = file_list.pop()["file_id"]
+
+    # Set sample payload.
+    sample_list, _ = list_object_detection_sample_payload
+    for sample in sample_list:
+        sample["project_id"] = project_id
+        sample["file_id"] = file_id
+
+        # Create sample.
+        sample_response = client.post(url=f"/projects/{project_id}/samples/", json=sample)
+        assert sample_response.status_code == 201, f"Failed to create sample: {sample_response.text}"
+        created_sample_data = sample_response.json()
+        created_sample_id = created_sample_data["_id"]
+
+        # Update the created sample with new coordinates.
+        updated_sample_data = {"cx": random.uniform(0.0, 1.0), "cy": random.uniform(0.0, 1.0)}
+
+        # Update the sample with wrong authentication (no authentication).
+        update_response = client.put(
+            url=f"/projects/{second_project_id}/samples/{created_sample_id}", json=updated_sample_data
+        )
+        assert update_response.status_code == 404, (
+            f"Failed to not update sample with wrong project ID: {update_response.text}"
+        )
+        assert (
+            update_response.json()["detail"]
+            == f"Sample with ID {created_sample_id} does not belong to project with ID {second_project_id}."
+        )
+
+
+def test_delete_object_detection_sample(
+    client: TestClient,
+    image_project_payload: dict,
+    image_file_payload: list[tuple[str, tuple[str, bytes, str]]],
+    list_object_detection_sample_payload: tuple[list[dict], list[str]],
+    reset_file_directory: None,  # Used to reset file directory
+) -> None:
+    """
+    Test to delete an object detection sample.
+    """
+    # Create project first.
+    project_response = client.post(url="/projects/", json=image_project_payload)
+    assert project_response.status_code == 201, f"Failed to create project: {project_response.text}"
+    project = project_response.json()
+    project_id = project["_id"]
+
+    # Create file record.
+    file_response = client.post(url=f"/projects/{project_id}/files/", files=image_file_payload)
+    assert file_response.status_code == 201, f"Failed to create file: {file_response.text}"
+    file_list = file_response.json()["data"]
+    assert len(file_list) == 1
+    file_id = file_list.pop()["file_id"]
+
+    # Set sample payload.
+    sample_list, _ = list_object_detection_sample_payload
+    for sample in sample_list:
+        sample["project_id"] = project_id
+        sample["file_id"] = file_id
+
+        # Create sample.
+        sample_response = client.post(url=f"/projects/{project_id}/samples/", json=sample)
+        assert sample_response.status_code == 201, f"Failed to create sample: {sample_response.text}"
+        created_sample_data = sample_response.json()
+        created_sample_id = created_sample_data["_id"]
+
+        # Delete the created sample.
+        delete_response = client.delete(url=f"/projects/{project_id}/samples/{created_sample_id}")
+        assert delete_response.status_code == 204, f"Failed to delete sample: {delete_response.text}"
+
+        # Get the deleted sample.
+        get_response = client.get(url=f"/projects/{project_id}/samples/{created_sample_id}")
+        assert get_response.status_code == 404, f"Failed to not get deleted sample: {get_response.text}"
+        assert get_response.json()["detail"] == f"Sample with ID {created_sample_id} does not exist."
+
+
+def test_delete_object_detection_sample_with_wrong_project_id(
+    client: TestClient,
+    image_project_payload: dict,
+    image_file_payload: list[tuple[str, tuple[str, bytes, str]]],
+    list_object_detection_sample_payload: tuple[list[dict], list[str]],
+    reset_file_directory: None,  # Used to reset file directory
+) -> None:
+    """
+    Test to delete an object detection sample with wrong project ID.
+    """
+    # Create project first.
+    project_response = client.post(url="/projects/", json=image_project_payload)
+    assert project_response.status_code == 201, f"Failed to create project: {project_response.text}"
+    project = project_response.json()
+    project_id = project["_id"]
+
+    # Create second project to get its ID.
+    second_image_project_payload = image_project_payload.copy()
+    second_image_project_payload["name"] = "Second Project"
+
+    second_project_response = client.post(url="/projects/", json=second_image_project_payload)
+    assert second_project_response.status_code == 201, (
+        f"Failed to create second project: {second_project_response.text}"
+    )
+    second_project = second_project_response.json()
+    second_project_id = second_project["_id"]
+
+    # Create file record.
+    file_response = client.post(url=f"/projects/{project_id}/files/", files=image_file_payload)
+    assert file_response.status_code == 201, f"Failed to create file: {file_response.text}"
+    file_list = file_response.json()["data"]
+    assert len(file_list) == 1
+    file_id = file_list.pop()["file_id"]
+
+    # Set sample payload.
+    sample_list, _ = list_object_detection_sample_payload
+    for sample in sample_list:
+        sample["project_id"] = project_id
+        sample["file_id"] = file_id
+
+        # Create sample.
+        sample_response = client.post(url=f"/projects/{project_id}/samples/", json=sample)
+        assert sample_response.status_code == 201, f"Failed to create sample: {sample_response.text}"
+        created_sample_data = sample_response.json()
+        created_sample_id = created_sample_data["_id"]
+
+        # Delete the created sample with wrong authentication (no authentication).
+        delete_response = client.delete(url=f"/projects/{second_project_id}/samples/{created_sample_id}")
+        assert delete_response.status_code == 404, (
+            f"Failed to not delete sample with wrong project ID: {delete_response.text}"
+        )
+        assert (
+            delete_response.json()["detail"]
+            == f"Sample with ID {created_sample_id} does not belong to project with ID {second_project_id}."
+        )
+
+
+def test_delete_file_with_object_detection_samples(
+    client: TestClient,
+    image_project_payload: dict,
+    image_file_payload: list[tuple[str, tuple[str, bytes, str]]],
+    list_object_detection_sample_payload: tuple[list[dict], list[str]],
+    reset_file_directory: None,  # Used to reset file directory
+):
+    """
+    Test to delete a file with object detection samples.
+    """
+    # Create project first.
+    project_response = client.post(url="/projects/", json=image_project_payload)
+    assert project_response.status_code == 201, f"Failed to create project: {project_response.text}"
+    project = project_response.json()
+    project_id = project["_id"]
+
+    # Create file record.
+    file_response = client.post(url=f"/projects/{project_id}/files/", files=image_file_payload)
+    assert file_response.status_code == 201, f"Failed to create file: {file_response.text}"
+    file_list = file_response.json()["data"]
+    assert len(file_list) == 1
+    file_id = file_list.pop()["file_id"]
+
+    # Set sample payload.
+    sample_list, _ = list_object_detection_sample_payload
+    for sample in sample_list:
+        sample["project_id"] = project_id
+        sample["file_id"] = file_id
+
+        # Create sample.
+        sample_response = client.post(url=f"/projects/{project_id}/samples/", json=sample)
+        assert sample_response.status_code == 201, f"Failed to create sample: {sample_response.text}"
+
+    # Get all samples of the current project.
+    get_samples_response = client.get(url=f"/projects/{project_id}/samples/")
+    assert get_samples_response.status_code == 200, f"Failed to get samples: {get_samples_response.text}"
+    samples = get_samples_response.json()
+    assert len(samples) == len(sample_list)
+
+    # Delete the file.
+    delete_file_response = client.delete(url=f"/projects/{project_id}/files/{file_id}")
+    assert delete_file_response.status_code == 204, f"Failed to delete file: {delete_file_response.text}"
+
+    # Get all samples of the current project again to check if they were deleted.
+    get_samples_response = client.get(url=f"/projects/{project_id}/samples/")
+    assert get_samples_response.status_code == 200, (
+        f"Failed to get samples after deleting file: {get_samples_response.text}"
+    )
+    samples = get_samples_response.json()
+    assert len(samples) == 0
