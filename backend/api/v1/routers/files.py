5e1b8b8bdf055a6fd8f8843934a14071adca424e
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2026-02-18 19:55:57 -0300

    ✨ feat: Return the worker task model with it ID.

--- backend/api/v1/routers/files.py
+++ backend/api/v1/routers/files.py
@@ -3,6 +3,7 @@ Module with all endpoints related to file operations.
 """
 
 from fastapi import APIRouter, Depends, UploadFile, status
+from fastapi.exceptions import HTTPException
 from fastapi.params import File, Param
 from fastapi.requests import Request
 from fastapi.responses import Response
@@ -10,7 +11,13 @@ from pymongo.asynchronous.database import AsyncDatabase
 
 from backend.api.v1.models.files import AudioFile, ImageFile, TextFile, UploadedFileListResponse
 from backend.api.v1.models.projects import Project
-from backend.api.v1.utils.files import create_file_records, delete_file_records, get_files, load_file_content_by_id
+from backend.api.v1.models.worker_tasks import WorkerTaskResult
+from backend.api.v1.utils.files import (
+    delete_file_records,
+    get_files,
+    load_file_content_by_id,
+    push_upload_file_to_redis_queue,
+)
 from backend.api.v1.utils.projects import get_authenticated_project
 from backend.database.configs import DatabaseConfig
 from backend.limiter import limiter
@@ -64,9 +71,9 @@ async def get_file_data_endpoint(
     return await load_file_content_by_id(file_id=file_id, project_id=project_id, db=db)  # type: ignore
 
 
-@router.post(path="/", response_model=UploadedFileListResponse, status_code=status.HTTP_201_CREATED)
+@router.post(path="/", response_model=UploadedFileListResponse, status_code=status.HTTP_201_CREATED, deprecated=True)
 @limiter.limit("10/minute")
-async def upload_file_endpoint(
+async def _upload_file_endpoint(
     request: Request,
     file_list: UploadFile | list[UploadFile] = File(...),  # type: ignore
     project: Project = Depends(dependency=get_authenticated_project),
@@ -82,12 +89,29 @@ async def upload_file_endpoint(
     Returns:
             UploadedFileListResponse: The uploaded files.
     """
+    raise HTTPException(
+        status_code=status.HTTP_410_GONE, detail="This endpoint is deprecated. Please use the /queue endpoint instead."
+    )
+
+
+@router.post(path="/queue", response_model=WorkerTaskResult, status_code=status.HTTP_202_ACCEPTED)
+async def upload_file_endpoint(
+    file_list: UploadFile | list[UploadFile] = File(...),  # type: ignore
+    project: Project = Depends(dependency=get_authenticated_project),
+) -> WorkerTaskResult:
+    """
+    Endpoint to queue a file for processing.
+
+    Args:
+            file_list (UploadFile | list[UploadFile]): The file or list of files to queue.
+    """
     # Get project ID.
     project_id = project.id
 
-    # Process files.
-    data = await create_file_records(file_list=file_list, project_id=project_id, db=db)
-    return UploadedFileListResponse(data=data)  # type: ignore
+    # Push file to Redis queue.
+    result = await push_upload_file_to_redis_queue(file_list=file_list, project_id=project_id)
+
+    return WorkerTaskResult(task_id=result.id, message="File(s) has been queued for processing.")  # type: ignore
 
 
 @router.delete(path="/{file_id}", status_code=status.HTTP_204_NO_CONTENT)
