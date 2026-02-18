e1ad768dadac5f4267a3987daf8698a80281dd09
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2026-02-18 20:47:44 -0300

    ✨ feat: Add model schema to be used to return the status of a task.

--- backend/api/v1/models/worker_tasks.py
+++ backend/api/v1/models/worker_tasks.py
@@ -2,6 +2,8 @@
 Module with all models related to all worker tasks.
 """
 
+from typing import Any
+
 from pydantic import BaseModel, Field
 
 
@@ -14,3 +16,13 @@ class WorkerTaskResult(BaseModel):
     # Fields.
     task_id: str = Field(..., description="The ID of the worker task.")
     message: str = Field(..., description="The message associated with the worker task result.")
+
+
+class WorkerTaskStatus(BaseModel):
+    """
+    Model to represent the status of a worker task.
+    """
+
+    # Fields.
+    status: str = Field(..., description="The status of the worker task.")
+    results: Any | None = Field(default=None, description="The results of the worker task, if available.")
