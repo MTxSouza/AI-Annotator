5efc11e420c2b581ad388c49db970d21a081f5f2
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2026-02-20 22:12:39 -0300

    ✅ test: Add test to check the audio transcription sample update.

--- tests/backend/02_audio_transcription_samples_test.py
+++ tests/backend/02_audio_transcription_samples_test.py
@@ -188,3 +188,66 @@ def test_create_audio_transcription_sample_with_nonexistent_file(
         f"Failed to not create sample with nonexistent file ID: {sample_response.text}"
     )
     assert sample_response.json()["detail"] == f"File with ID {single_audio_transcription['file_id']} does not exist."
+
+
+def test_update_audio_transcription_sample(
+    client: TestClient,
+    audio_transcription_project_payload: dict,
+    audio_transcription_sample_payload: tuple[list[tuple[str, tuple[str, io.BytesIO, str]]], list[dict]],
+    reset_file_directory: None,  # Used to reset file directory
+):
+    """
+    Test to update an audio transcription sample.
+    """
+    # Unpack sample payload.
+    list_wav_audio_file, audio_transcription_list = audio_transcription_sample_payload
+
+    # Create a project.
+    project_response = client.post(url="/projects/", json=audio_transcription_project_payload)
+    assert project_response.status_code == 201, f"Failed to create project: {project_response.text}"
+    project = project_response.json()
+    project_id = project["_id"]
+
+    # Create file record.
+    worker_response = client.post(url=f"/projects/{project_id}/files/", files=list_wav_audio_file)
+    assert worker_response.status_code == 202, f"Failed to create file record: {worker_response.text}"
+
+    # Wait for worker task to complete.
+    worker_response_json = worker_response.json()
+    assert "task_id" in worker_response_json, "Response does not contain task_id"
+    worker_task_id = worker_response_json["task_id"]
+    file_data_list = check_for_worker_task_completion(client=client, worker_task_id=worker_task_id)
+
+    # Get file ID from response.
+    file_id_list = []
+    for file_data in file_data_list:
+        assert file_data["status"] == "Created"
+        file_id = file_data["file_id"]
+        file_id_list.append(file_id)
+    assert len(file_id_list) > 0, "File IDs not found in response"
+
+    # Create sample records.
+    sample_id_list = []
+    for i in range(len(audio_transcription_list)):
+        current_audio_transcription_sample = audio_transcription_list[i]
+        current_audio_transcription_sample["project_id"] = project_id
+        current_audio_transcription_sample["file_id"] = file_id_list[i]
+        sample_response = client.post(url=f"/projects/{project_id}/samples/", json=current_audio_transcription_sample)
+
+        assert sample_response.status_code == 201, f"Failed to create sample: {sample_response.text}"
+        sample_id_list.append(sample_response.json()["_id"])
+
+    # Update sample records.
+    for i in range(len(sample_id_list)):
+        sample_id = sample_id_list[i]
+        updated_text = f"Updated transcription for audio file {i}"
+        update_payload = {"text": updated_text}
+        update_response = client.put(url=f"/projects/{project_id}/samples/{sample_id}/", json=update_payload)
+        assert update_response.status_code == 201, f"Failed to update sample: {update_response.text}"
+
+        # Check response.
+        sample_response_json = update_response.json()
+        assert sample_response_json["_id"] == sample_id
+        assert sample_response_json["project_id"] == project_id
+        assert sample_response_json["file_id"] == file_id_list[i]
+        assert sample_response_json["text"] == updated_text
