f66e7a009d527d68279c0ca085676e2e3e7d35f8
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2026-02-23 18:02:45 -0300

    ✨ feat: Remove unnecessary component for frontend.

--- frontend/src/components/ApplicationContainer.tsx
+++
@@ -1,6 +0,0 @@
-import { JSX } from 'react'
-import '../styles/ApplicationContainer.css'
-
-export function ApplicationContainer(): JSX.Element {
-    return <div className="application-container-component"></div>
-}
