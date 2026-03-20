75e48422dfb09090ad34514a165c856a242dcd09
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2026-03-19 22:12:24 -0300

    ✨ feat: Add initial files to create the confirm project password popup component.

---
+++ frontend/src/components/ConfirmProjectPasswordPopup.tsx
@@ -0,0 +1,44 @@
+import { JSX } from 'react'
+import { APIErrorResponse } from '../scripts/common'
+import { PopupOverlay } from '../components/PopupOverlay'
+import { useErrorDialog } from '../components/ErrorDialog'
+import { authenticateProjectRequest } from '../scripts/projects'
+
+import '../styles/ConfirmProjectPasswordPopup.css'
+
+export function ConfirmProjectPasswordPopup({
+    projectId,
+    password,
+    closePopup,
+    onSuccess,
+}: {
+    projectId: string
+    password: string
+    closePopup: () => void
+    onSuccess: () => void
+}): JSX.Element {
+    console.info('Opening confirm project password popup...')
+
+    // Set up error dialog.
+    const { showErrorDialog } = useErrorDialog()
+
+    // Authenticate project function.
+    async function authenticateProject(projectId: string, password: string) {
+        try {
+            await authenticateProjectRequest(projectId, password)
+            onSuccess()
+        } catch (error) {
+            if (error instanceof APIErrorResponse) {
+                console.error('Error authenticating to project:', error)
+                showErrorDialog(error.message, error.status_code)
+            } else {
+                console.error('Unexpected error authenticating to project:', error)
+                showErrorDialog('An unexpected error occurred while authenticating to the project.', 500)
+            }
+        }
+    }
+
+    const component = <div className="confirm-project-password-popup-component"></div>
+
+    return <PopupOverlay children={component} />
+}
