12336343f393a9fc73ee08abd4363b74efb867d2
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2026-04-09 19:34:47 -0300

    ✨ feat: Set two main pages for project.

--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -1,12 +1,12 @@
 /*
-Main dataset page for project management.
+Main settings page for project management.
 */
 import { JSX } from 'react'
 import { Project } from '../scripts/projects'
 import { useOutletContext } from 'react-router-dom'
 
 // Components.
-export function Dataset(): JSX.Element {
+export function Settings(): JSX.Element {
     // Get the project data from the outlet context.
     const project = useOutletContext<Project>()
 
