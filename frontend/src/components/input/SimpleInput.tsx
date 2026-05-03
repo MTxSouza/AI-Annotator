7c0f7ba681f368ad8ca384dac24b4de77ff58e6a
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2026-05-03 09:43:43 -0300

    ✨ feat: Set style for input component and allow set a custom default value for component.

--- frontend/src/components/input/SimpleInput.tsx
+++ frontend/src/components/input/SimpleInput.tsx
@@ -1,20 +1,34 @@
 import { JSX } from 'react'
 
+import '../../styles/input/SimpleInput.css'
+
 export function SimpleInput({
     id,
     placeholder,
     onChangeEvent,
+    value = '',
     type = 'text',
     maxLength = 32,
 }: {
     id: string
     placeholder: string
     onChangeEvent: (value: any) => void
+    value?: string
     type?: string
     maxLength?: number
 }): JSX.Element {
     /*
     Abstracted input component for text input.
     */
-    return <input id={id} type={type} maxLength={maxLength} placeholder={placeholder} onChange={onChangeEvent} />
+    return (
+        <input
+            className="simple-input-component"
+            id={id}
+            value={value}
+            type={type}
+            maxLength={maxLength}
+            placeholder={placeholder}
+            onChange={onChangeEvent}
+        />
+    )
 }
