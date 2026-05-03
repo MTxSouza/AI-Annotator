b427f962c0f9a8571d421e628330e92381ba07f4
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2026-05-03 08:39:32 -0300

    💄 style: Set main page ID to represent the main page component.

--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -5,10 +5,12 @@ import { JSX } from 'react'
 import { Project } from '../scripts/projects'
 import { useOutletContext } from 'react-router-dom'
 
+import '../styles/pages/Settings.css'
+
 // Components.
 export function Settings(): JSX.Element {
     // Get the project data from the outlet context.
     const project = useOutletContext<Project>()
 
-    return <div className="main-page-component"></div>
+    return <div id="main-page" className="project-settings-component"></div>
 }
