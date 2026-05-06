4da4e67e83f66be96e7688ecfabb5d94b71b188c
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2026-05-06 10:53:14 -0300

    ✨ feat: Open delete project popup to delete a project inside of it.

--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -3,15 +3,20 @@ Main settings page for project management.
 */
 import { useState, useEffect, JSX } from 'react'
 import { Project } from '../scripts/projects'
-import { useOutletContext } from 'react-router-dom'
+import { useNavigate, useOutletContext } from 'react-router-dom'
 import { SimpleInput } from '../components/input/SimpleInput'
 import { Button } from '../components/button/Button'
 import { ButtonType } from '../scripts/Button'
+import { ConfirmProjectDeletionPopup } from '../components/popup/ConfirmProjectDeletionPopup'
+import { PROJECT_MENU_URL, redirectTo } from '../scripts/common'
 
 import '../styles/pages/Settings.css'
 
 // Components.
 export function Settings(): JSX.Element {
+    // Set page navigator.
+    const navigate = useNavigate()
+
     // Get the project data from the outlet context.
     const project = useOutletContext<Project>()
     const currentProjectName = project.name
@@ -19,6 +24,7 @@ export function Settings(): JSX.Element {
     // Set up states.
     const [newProjectName, setNewProjectName] = useState<string>(currentProjectName)
     const [isProjectInfoChanged, setIsProjectInfoChanged] = useState<boolean>(false)
+    const [deleteProject, setDeleteProject] = useState<boolean>(false)
 
     useEffect(() => {
         setIsProjectInfoChanged(currentProjectName !== newProjectName)
@@ -62,7 +68,7 @@ export function Settings(): JSX.Element {
                     id="project-delete-btn"
                     value="delete project"
                     buttonType={ButtonType.TERTIARY}
-                    onClickEvent={() => console.log('Delete project clicked')}
+                    onClickEvent={() => setDeleteProject(true)}
                 />
                 <Button
                     id="project-settings-save-btn"
@@ -72,6 +78,15 @@ export function Settings(): JSX.Element {
                     disabled={!isProjectInfoChanged}
                 />
             </div>
+
+            {deleteProject && (
+                <ConfirmProjectDeletionPopup
+                    projectId={project._id}
+                    isPrivate={project.is_private}
+                    closePopup={() => setDeleteProject(false)}
+                    refreshProjects={() => redirectTo(PROJECT_MENU_URL, navigate)}
+                />
+            )}
         </div>
     )
 }
