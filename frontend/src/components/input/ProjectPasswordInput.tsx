c7cb2156e6acbf573202d0708530062eb94d2f40
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2026-05-07 23:03:00 -0300

    ✨ feat: Add extra parameters to control the placeholder and id of input field.

--- frontend/src/components/input/ProjectPasswordInput.tsx
+++ frontend/src/components/input/ProjectPasswordInput.tsx
@@ -7,25 +7,36 @@ import EyeOpen from '../../icons/eyeOpen.svg?react'
 import '../../styles/input/ProjectPasswordInput.css'
 
 export function ProjectPasswordInput({
+    id,
     isOptional,
     setProjectPassword,
+    placeholder,
 }: {
+    id?: string
     isOptional?: boolean
     setProjectPassword: (password: string | null) => void
+    placeholder?: string
 }): JSX.Element {
     // Set up state to manage the visibility of the project password input.
     const [hidePassword, setHidePassword] = useState<boolean>(true)
 
+    // Set ID.
+    const inputId = id || 'project-password-input-component'
+
     // Set placeholder message.
     let placeholderMessage = 'Password'
-    if (isOptional) {
-        placeholderMessage += ' (optional)'
+    if (placeholder === undefined) {
+        if (isOptional) {
+            placeholderMessage += ' (optional)'
+        }
+    } else {
+        placeholderMessage = placeholder
     }
 
     const component = (
         <div className="project-password-input-container">
             <SimpleInput
-                id="project-password-input-component"
+                id={inputId}
                 type={hidePassword ? 'password' : 'text'}
                 placeholder={placeholderMessage}
                 onChangeEvent={(event) => setProjectPassword(event.target.value || null)}
