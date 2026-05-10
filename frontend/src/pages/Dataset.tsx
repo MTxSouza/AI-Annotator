6a716b8e7a22ecc1069a37be71c21d22347f4415
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2026-05-10 19:53:11 -0300

    ✨ feat: Set up initial dataset page and allow upload files from ui.

--- frontend/src/pages/Dataset.tsx
+++ frontend/src/pages/Dataset.tsx
@@ -3,20 +3,66 @@ Main dataset page for project management.
 */
 import { JSX, useEffect, useState } from 'react'
 import { Project } from '../scripts/projects'
+import { File } from '../scripts/files'
 import { fetchData, RequestMethod, APIErrorResponse } from '../scripts/common'
 import { useOutletContext } from 'react-router-dom'
 import { FileUploadArea } from '../components/FileUploadArea'
 import { useDialog } from '../components/dialog/Dialog'
+import { UploadFileCard, FileCard } from '../components/FileCard'
 
 import '../styles/pages/Dataset.css'
 
 // Components.
+function DatasetInfoHeaderCard({ value, label }: { value: number | string; label: string }): JSX.Element {
+    return (
+        <div className="dataset-info-header-card-component">
+            <span>{value}</span>
+            <p>{label}</p>
+        </div>
+    )
+}
+
 export function Dataset(): JSX.Element {
     // Set up dialog.
     const { showDialog } = useDialog()
 
     // Set up states.
     const [projectFiles, setProjectFiles] = useState<File[]>([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    async function uploadFiles(files: FileList | null) {
+        // Ignore if no files are selected.
+        if (!files || files.length === 0) {
+            return
+        }
+
+        // Create form data for file upload.
+        const formData = new FormData()
+        for (let i = 0; i < files.length; i++) {
+            formData.append('file_list', files[i])
+        }
+        console.debug(`Uploading ${files.length} files to project ${project._id}`)
+
+        // Send upload request to the server.
+        try {
+            const uploadWorker = await fetchData(
+                `/projects/${project._id}/files`,
+                RequestMethod.POST,
+                undefined,
+                formData,
+            )
+            showDialog('info', 'Files uploaded requested successfully!', null)
+            console.debug('Upload worker response:', uploadWorker)
+        } catch (error) {
+            if (error instanceof APIErrorResponse) {
+                console.error('Error uploading files:', error)
+                showDialog('error', error.message, error.status_code)
+            } else {
+                console.error('Unexpected error uploading files:', error)
+                showDialog('error', 'An unexpected error occurred while uploading files.', 500)
+            }
+        }
+    }
 
     // Get the project data from the outlet context.
     const project = useOutletContext<Project>()
@@ -29,6 +75,7 @@ export function Dataset(): JSX.Element {
                 const projectFiles = await fetchData(`/projects/${project._id}/files`, RequestMethod.GET)
                 console.debug(`Fetched ${projectFiles.length} files for project ${project._id}`)
                 setProjectFiles(projectFiles)
+                setIsLoading(false)
             } catch (error) {
                 if (error instanceof APIErrorResponse) {
                     console.error('Error fetching project files:', error)
@@ -42,16 +89,35 @@ export function Dataset(): JSX.Element {
         fetchProjectFiles()
     }, [project._id])
 
+    // Wait fetching project files before rendering.
+    if (isLoading) {
+        return <></>
+    }
+
+    // Compute total disk usage of project files.
+    const totalDiskUsage = projectFiles.reduce((total, file) => total + file.size_in_bytes, 0) / (1024 * 1024) // Convert to megabytes
+    console.debug(`Total disk usage for project ${project._id}: ${totalDiskUsage} MB`)
+
     return (
         <div className="project-dataset-component">
-            {!projectFiles.length && (
+            <div className="project-dataset-header-component">
+                <DatasetInfoHeaderCard value={projectFiles.length} label="Files" />
+                <DatasetInfoHeaderCard value={project.details.number_of_samples} label="Total annotated samples" />
+                <DatasetInfoHeaderCard value={totalDiskUsage.toFixed(2)} label="Total disk usage (MB)" />
+            </div>
+            {(!projectFiles.length && (
                 <FileUploadArea
                     displayName="Upload a file to start labeling."
-                    onUpload={() => {
-                        console.debug('Upload button clicked.')
-                    }}
+                    onUpload={uploadFiles}
                     allowedFileTypes={projectFileFormat}
                 />
+            )) || (
+                <div className="project-file-list-component">
+                    <UploadFileCard onUpload={uploadFiles} allowedFileTypes={projectFileFormat} />
+                    {projectFiles.map((file) => (
+                        <FileCard key={file._id} file={file} />
+                    ))}
+                </div>
             )}
         </div>
     )
